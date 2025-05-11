2b3ef6c1796044e2627922549fe2fca027a6ddd2
tenmongit <tenmonwork@outlook.com>
2025-05-11 20:29:03 +0000

    123

---
+++ client/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { tempo } from "tempo-devtools/dist/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tempo(), // Add the tempo plugin
+  ],
+  server: {
+    // @ts-ignore
+    allowedHosts: process.env.TEMPO === "true" ? true : undefined,
+  },
+});
